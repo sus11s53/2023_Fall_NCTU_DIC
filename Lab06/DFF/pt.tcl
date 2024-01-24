b62e6f65b5ed744f984cbdbca464b78121d770c0
sus11s53 <113005753+sus11s53@users.noreply.github.com>
2024-01-24 15:34:03 +0800

    Add files via upload

---
+++ Lab06/DFF/pt.tcl
@@ -0,0 +1,48 @@
+# specify parameters
+set DESIGN "Convolution"
+
+#Step 1 Read in the design data, which includes a gate-level netlist and associated logic libraries.
+set search_path " ../02_SYN/Netlist \
+                  ./          \
+                  " 
+
+#set link_library { slow.db dw_foundation.sldb }
+set link_path "* asap7sc7p5t_INVBUF_RVT_TT_08302018.db asap7sc7p5t_SIMPLE_RVT_TT_08302018.db asap7sc7p5t_SEQ_RVT_TT_08302018.db"
+read_verilog Netlist/$DESIGN\_SYN.v
+current_design $DESIGN
+
+
+link_design -keep_sub_designs $DESIGN
+
+#create_clock -period $CLK_period CLK
+read_sdc Netlist/$DESIGN\_SYN.sdc
+
+#Step 6 Specify case and mode analysis settings.
+#Step 7 Back-annotate delay and parasitics.
+read_sdf Netlist/$DESIGN\_SYN.sdf
+
+# set_annotated_check 
+# modify here 
+
+
+foreach_in_collection x [get_cell */x1/A1_reg] {
+  set_annotated_check -0 -setup -from [get_object_name $x]/CLK -to [get_object_name $x]/D -clock rise
+  set_annotated_check -0 -hold  -from [get_object_name $x]/CLK -to [get_object_name $x]/D -clock rise
+}
+write_sdf Netlist/$DESIGN\_SYN_pt.sdf
+
+#Step 8 (Optional) Apply variation.
+#Step 9 Specify power information
+#Step 10 (Optional) Specify options and data for signal integrity analysis.
+#Step 11 (Optional) Apply options for specific design techniques.
+#Step 12 Check the design data and analysis setup.
+check_timing
+
+#Step 13 Perform a full timing analysis and examine the results.
+report_timing
+
+#Step 14 (Optional) Perform ECO to fix timing violations and recover power.
+#Step 15 Save the PrimeTime session.
+#save_session
+exit
+
